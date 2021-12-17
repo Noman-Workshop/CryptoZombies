201984fa5a3d088c7d25446c28e1dc73d8e00a88
Noman5237 <anonyman637@gmail.com>
2021-12-17 12:29:21 +0600

    end of l1c11

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -1,17 +1,19 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
-	uint256 dnaDigits = 16;
-	uint256 dnaModulus = 10**dnaDigits;
+	uint dnaDigits = 16;
+	uint dnaModulus = 10**dnaDigits;
 
 	struct Zombie {
 		string name;
-		uint256 dna;
+		uint dna;
 	}
 
 	Zombie[] public zombies;
 
-	function _createZombie(string memory _name, uint256 _dna) private {
+	function _createZombie(string memory _name, uint _dna) private {
 		zombies.push(Zombie(_name, _dna));
 	}
+
+	function _generateRandomDna(string memory _str) private view returns (uint) {}
 }
